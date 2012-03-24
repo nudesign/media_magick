39c7ccbd5c96d8de4d8952580419437b278cb3f6
Tiago Rafael Godinho <tiagogodinho3@gmail.com>
2012-03-24 18:51:54 -0300

    RSpec configuration.

---
+++ spec/spec_helper.rb
@@ -0,0 +1,9 @@
+require 'media_magick'
+
+RSpec.configure do |config|
+  # Remove this line if you don"t want RSpec"s should and should_not
+  # methods or matchers
+  require 'rspec/expectations'
+  
+  config.include RSpec::Matchers
+end
