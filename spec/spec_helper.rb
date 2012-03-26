ba1894b38485b30fbb4796b0d0ee4536d3f45e7d
Tiago Rafael Godinho <tiagogodinho3@gmail.com>
2012-03-26 11:22:00 -0300

    Clean database after tests.

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -6,4 +6,19 @@ RSpec.configure do |config|
   require 'rspec/expectations'
   
   config.include RSpec::Matchers
+  
+  config.before :suite do
+    Mongoid.configure do |config|
+      config.master = Mongo::Connection.new.db('media_magick')
+    end
+  end
+  
+  config.after :each do
+    Mongoid.master.collections.select { |c| c.name !~ /^system/ }.each(&:drop)
+  end
+  
+  config.after :suite do
+    Mongoid.master.connection.drop_database('media_magick')
+    Mongoid.master.connection.close
+  end
 end
