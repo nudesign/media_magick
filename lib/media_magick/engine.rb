7bb32a9613d9302efac6126402c99acf9c1398a7
Tiago Rafael Godinho <tiagogodinho3@gmail.com>
2012-03-30 11:19:50 -0300

    Include MediaHelper on ActionView::Base.

--- lib/media_magick/engine.rb
+++ lib/media_magick/engine.rb
@@ -1,5 +1,9 @@
 module MediaMagick
   class Engine < ::Rails::Engine
     isolate_namespace MediaMagick
+    
+    initializer 'media_magick.include_helper' do
+      ActionView::Base.send :include, MediaMagickHelper
+    end
   end
 end
