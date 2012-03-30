88f039f92ce3e99e258fcaad46d08deda5c538d6
Tiago Rafael Godinho <tiagogodinho3@gmail.com>
2012-03-30 11:23:04 -0300

    Creating a thumb on AttachmentUploader.

--- lib/media_magick/attachment_uploader.rb
+++ lib/media_magick/attachment_uploader.rb
@@ -2,7 +2,7 @@ class AttachmentUploader < CarrierWave::Uploader::Base
 
   # Include RMagick or MiniMagick support:
   # include CarrierWave::RMagick
-  # include CarrierWave::MiniMagick
+  include CarrierWave::MiniMagick
 
   # Choose what kind of storage to use for this uploader:
   storage :file
@@ -27,9 +27,9 @@ class AttachmentUploader < CarrierWave::Uploader::Base
   # end
 
   # Create different versions of your uploaded files:
-  # version :thumb do
-  #   process :scale => [50, 50]
-  # end
+  version :thumb do
+    process :resize_to_fit => [156, 156]
+  end
 
   # Add a white list of extensions which are allowed to be uploaded.
   # For images you might use something like this:
