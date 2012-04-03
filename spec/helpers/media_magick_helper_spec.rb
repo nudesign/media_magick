c94d554811a01db821ae4f304afb41eda22f053e
Lucas Renan <lucas@freelancersbrasil.com>
2012-04-03 17:25:29 -0300

    adds a test to attachment_container helper for a better coverage

--- spec/helpers/media_magick_helper_spec.rb
+++ spec/helpers/media_magick_helper_spec.rb
@@ -9,5 +9,19 @@ describe MediaMagickHelper do
       helper.attachment_container(album, :photos) do
       end.should eq('<div class="attachmentUploader photos" data-id="12345678" data-model="album" data-relation="photos" id="album-photos"></div>')
     end
+
+    it 'should renders default partial if block is not given' do
+      photo = AlbumPhotos.new
+      photo.stub(filename: 'photo.jpg', url: 'url/photo.jpg')
+      
+      file = AlbumFiles.new
+      file.stub(filename: 'file.pdf', url: 'url/file.pdf')
+
+      album = Album.new(photos: [photo], files: [file])
+      album.stub(id: '12345678')
+      
+      helper.attachment_container(album, :photos).should match(/url\/photo.jpg/)
+      helper.attachment_container(album, :files).should match(/url\/file.pdf/)
+    end
   end
 end
