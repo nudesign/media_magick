741c2d7c66fabdd1bc81ba6a50e849803478cdbe
Tiago Rafael Godinho <tiagogodinho3@gmail.com>
2012-04-06 18:31:40 -0300

    Creating tests for routes.

---
+++ spec/routing/attach_spec.rb
@@ -0,0 +1,24 @@
+require 'spec_helper'
+
+describe 'routes for Attach' do
+  it 'routes /upload to create action' do
+    get('/upload').should route_to('attach#create')
+  end
+
+  it 'routes /remove to destroy action' do
+    get('/remove').should route_to('attach#destroy')
+  end
+
+  it 'routes /update_priority to update_priority action' do
+    get('/update_priority').should route_to('attach#update_priority')
+  end
+
+  it 'routes /recreate_versions to recreate_versions action' do
+    get('/recreate_versions').should route_to('attach#recreate_versions')
+  end
+
+  it 'routes recreate_versions_path to recreate_versions action' do
+    get(recreate_versions_path).should route_to('attach#recreate_versions')
+  end
+
+end
