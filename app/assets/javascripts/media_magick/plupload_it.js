16db0e5ee983d09bdca741010429bb7744eb2f14
Lucas Renan <lucas@freelancersbrasil.com>
2012-07-03 18:16:47 -0300

    removing .rvmrc

--- app/assets/javascripts/media_magick/plupload_it.js
+++ app/assets/javascripts/media_magick/plupload_it.js
@@ -93,18 +93,19 @@
           var $attachment = $(this).parents('.attachment');
           var $attachmentUploader = $(this).parents('.attachmentUploader');
 
-          $.get('/upload', {
-            model: $container.data('model'),
-            id: $container.data('id'),
-            relation: $container.data('relation'),
-            relation_id: $attachment.data('id'),
-            embedded_in_model: $attachmentUploader.data('embedded-in-model'),
-            embedded_in_id: $attachmentUploader.data('embedded-in-id'),
-            partial: $container.data('partial') === undefined ? '' : $container.data('partial'),
-            video: $(".attachmentVideoUploaderField").val()
-          }, function(data) {
-            $(".loadedAttachments").append(data);
-          });
+          console.log($container.data('relation'));
+          // $.get('/upload', {
+          //   model: $container.data('model'),
+          //   id: $container.data('id'),
+          //   relation: $container.data('relation'),
+          //   relation_id: $attachment.data('id'),
+          //   embedded_in_model: $attachmentUploader.data('embedded-in-model'),
+          //   embedded_in_id: $attachmentUploader.data('embedded-in-id'),
+          //   partial: $container.data('partial') === undefined ? '' : $container.data('partial'),
+          //   video: $(".attachmentVideoUploaderField").val()
+          // }, function(data) {
+          //   $(".loadedAttachments").append(data);
+          // });
         });
       });
 
