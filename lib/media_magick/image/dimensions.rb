26243b87905a5c3ae44b50cffdbcb716871832ba
lucas renan <contato@lucasrenan.com>
2012-12-03 20:44:29 -0200

    access image dimensions by string keys, even when returning 0

--- lib/media_magick/image/dimensions.rb
+++ lib/media_magick/image/dimensions.rb
@@ -9,13 +9,13 @@ module MediaMagick
         begin
           if model.dimensions[version_key.to_s].nil?
             image = MiniMagick::Image.open(file.path)
-            model.dimensions[version_key.to_s] = {width: image[:width], height: image[:height]}
+            model.dimensions[version_key.to_s] = {"width" => image[:width], "height" => image[:height]}
             model.save
           end
 
           return model.dimensions[version_key.to_s]
         rescue
-          return {width: 0, height: 0}
+          return {"width" => 0, "height" => 0}
         end
       end
     end
